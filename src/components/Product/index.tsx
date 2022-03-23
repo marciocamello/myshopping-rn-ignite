f8ba1c34140f67b5c746fbc70aa5e1ad5c9d84b9
marciocamello <marciocamello@oulook.com>
2022-03-23 16:07:35 +0000

    feat: add realtime list products and toogle done

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,42 +1,53 @@
+import firestore from '@react-native-firebase/firestore';
 import React from 'react';
-
 import { ButtonIcon } from '../ButtonIcon';
-import { Container, Info, Title, Quantity, Options } from './styles';
+import { Container, Info, Options, Quantity, Title } from './styles';
 
 export type ProductProps = {
-  id: string;
-  description: string;
-  quantity: number;
-  done: boolean;
+    id: string;
+    description: string;
+    quantity: number;
+    done: boolean;
 }
 
 type Props = {
-  data: ProductProps;
+    data: ProductProps;
 }
 
 export function Product({ data }: Props) {
-  return (
-    <Container>
-      <Info>
-        <Title done={data.done}>
-          {data.description}
-        </Title>
 
-        <Quantity>
-          Quantidade: {data.quantity}
-        </Quantity>
-      </Info>
+    function handleDoneToggle() {
+        firestore()
+            .collection('products')
+            .doc(data.id)
+            .update({
+                done: !data.done,
+            });
+    }
 
-      <Options>
-        <ButtonIcon
-          icon={data.done ? "undo" : "check"}
-        />
+    return (
+        <Container>
+            <Info>
+                <Title done={data.done}>
+                    {data.description}
+                </Title>
 
-        <ButtonIcon
-          icon="delete"
-          color="alert"
-        />
-      </Options>
-    </Container>
-  );
+                <Quantity>
+                    Quantidade: {data.quantity}
+                </Quantity>
+            </Info>
+
+            <Options>
+                <ButtonIcon
+                    icon={data.done ? "undo" : "check"}
+                    onPress={handleDoneToggle}
+                />
+
+                <ButtonIcon
+                    icon="delete"
+                    color="alert"
+                />
+            </Options>
+        </Container>
+    );
 }
--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -8,22 +8,21 @@ export function ShoppingList() {
     const [products, setProducts] = useState<ProductProps[]>([]);
 
     useEffect(() => {
-        firestore()
+        const subscribe = firestore()
             .collection('products')
-            .get()
-            .then(response => {
-                const data = response.docs.map(doc => {
+            .orderBy('description')
+            .onSnapshot((querySnapshot) => {
+                const data = querySnapshot.docs.map((doc) => {
                     return {
                         id: doc.id,
-                        ...doc.data()
+                        ...doc.data(),
                     };
                 }) as ProductProps[];
 
                 setProducts(data);
-            })
-            .catch(error => {
-                console.log(error);
             });
+
+        return () => subscribe();
     }, []);
 
     return (
